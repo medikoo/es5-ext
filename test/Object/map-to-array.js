49dd5ff462c2f0b7edb72f7c103099a2758d701f
Mariusz Nowak <medikoo@medikoo.com>
2011-07-15 08:40:56 +0200

    Object.mapToArray

---
+++ test/Object/map-to-array.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (t, a) {
+	t = t.call;
+	var o = { 1: 1, 2: 2, 3: 3 }
+	  , o2 = t(o, function (value, name) {
+			return value + Number(name);
+		});
+	a.deepEqual(o2, [2, 4, 6]);
+};
