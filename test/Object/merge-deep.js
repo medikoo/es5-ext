d0cb2f58e9986a65886b2aaf65bacdd62f3f59a4
Mariusz Nowak <medikoo@medikoo.com>
2011-07-18 08:38:11 +0200

    mergeDeep for objects

---
+++ test/Object/merge-deep.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var x, y, z;
+	t = t.call;
+	x = { raz: 1, dwa: 2 };
+	y = Object.create(x);
+	y.trzy = 3;
+	y.cztery = 4;
+	z = t({}, y);
+	a.deepEqual(z, { raz: 1, dwa: 2, trzy: 3, cztery: 4 });
+	a.ok(z.hasOwnProperty('raz'));
+	a.ok(z.hasOwnProperty('trzy'));
+};
