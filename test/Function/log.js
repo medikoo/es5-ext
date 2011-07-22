37e59052c144dc553cad5fc06d40877aafdad289
Mariusz Nowak <medikoo@medikoo.com>
2011-07-22 08:50:34 +0200

    Function log

---
+++ test/Function/log.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var noop    = require('../../lib/Function/noop')
+  , toArray = require('../../lib/List/to-array').call;
+
+module.exports = function (t, a) {
+	var log = [], fn, o = {};
+	fn = t(noop, log);
+
+	fn(1, 'raz', o);
+	fn(2, o, 'dwa');
+
+	a.deep(toArray(log[0]), [1, 'raz', o], 'First call');
+	a.deep(toArray(log[1]), [2, o, 'dwa'], 'Second call');
+	a(log.length, 2, 'Log length');
+};
