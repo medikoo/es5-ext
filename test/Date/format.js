f27c3543c685a2d413c8cd71acef1f96c16e69d1
Mariusz Nowak <medyk@medikoo.com>
2011-07-23 14:07:20 +0200

    -- ignore

--- test/Date/format.js
+++ test/Date/format.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (t, a) {
-	var d = new Date(2011, 2, 3, 3, 5, 5, 32);
+	var dt = new Date(2011, 2, 3, 3, 5, 5, 32);
 	t = t(' %Y.%y.%m.%d.%H.%M.%S.%L ').call;
-	a(t(d), ' 2011.11.03.03.03.05.05.032 ');
+	a(t(dt), ' 2011.11.03.03.03.05.05.032 ');
 };
