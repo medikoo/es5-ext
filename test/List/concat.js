33ca794add95fb2814ecdcdc967da2858288380a
Mariusz Nowak <medikoo@medikoo.com>
2011-08-06 20:43:21 +0200

    Concise rules for List.concat

--- test/List/concat.js
+++ test/List/concat.js
@@ -3,5 +3,5 @@
 exports.__generic = function (t, a) {
 	t = t.call;
 	a(t(this, 4, 5).length, 5, "As context");
-	a(t(this, 4, this, 5).length, 8, "As argument");
+	a(t(this, 4, this, 5).length, Array.isArray(this) ? 8 : 6, "As argument");
 };
