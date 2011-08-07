5a3a30415632878c2f369f797c0e095efe7a0962
Mariusz Nowak <medikoo@medikoo.com>
2011-08-07 12:54:08 +0200

    Functionalize aritize

--- test/Function/aritize.js
+++ test/Function/aritize.js
@@ -3,6 +3,7 @@
 var toArray = require('../../lib/List/to-array').call;
 
 module.exports = function (t, a) {
+	t = t.call;
 	a(t(function (a, b, c) {
 		return toArray(arguments).toString() + '|' + String(arguments.length);
 	}, 2)(1, 2, 3), '1,2|2');
