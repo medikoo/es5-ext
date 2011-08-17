f53dce4f4045b91c6cf73b560c27c4340df5f55c
Mariusz Nowak <medikoo@medikoo.com>
2011-08-17 23:12:18 +0200

    toArray on array should return same array

--- test/List/to-array.js
+++ test/List/to-array.js
@@ -4,11 +4,14 @@ module.exports = {
 	__generic: function (t, a) {
 		t = t.call;
 		a.ok(t(this) instanceof Array, "turned to an array");
-		a.equal(t(this).length, this.length, "length matches");
+		a(t(this).length, this.length, "length matches");
 	},
 	"": function (t, a) {
+		var o = [1, 2, 3];
 		t = t.call;
-		a(t([1,2,3,4,5], 2, 3).length, 5, "Extra arguments does not affect result");
-		a.deep(t([3]), [3], "List with one numeric value");
+		a(t("12345", 2, 3).length, 5, "Extra arguments does not affect result");
+		a.deep(t(function (x) { return arguments; }(3)), [3],
+			"List with one numeric value");
+		a(t(o), o, "On array returns same array");
 	}
 };
