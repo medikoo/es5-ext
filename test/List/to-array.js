dac24d16dfd89300a6f6104324cb6e93e452e012
Mariusz Nowak <medyk@medikoo.com>
2011-08-18 14:52:03 +0200

    Fixed toArray, it crashed on non Array and non Arguments objects

--- test/List/to-array.js
+++ test/List/to-array.js
@@ -7,11 +7,15 @@ module.exports = {
 		a(t(this).length, this.length, "length matches");
 	},
 	"": function (t, a) {
-		var o = [1, 2, 3];
+		var o = [1, 2, 3], r;
 		t = t.call;
 		a(t("12345", 2, 3).length, 5, "Extra arguments does not affect result");
 		a.deep(t(function (x) { return arguments; }(3)), [3],
 			"List with one numeric value");
 		a(t(o), o, "On array returns same array");
+
+		r = t({'0': 'raz', '1': 'dwa', length: 2 });
+		a.ok(r instanceof Array, "Dummy");
+		a.deep(r, ['raz', 'dwa'], "Dummy content");
 	}
 };
