effe783d8de371934dcf39ec7b3165c6d5bf1663
Mariusz Nowak <medyk@medikoo.com>
2011-08-19 12:51:40 +0200

    'getArray' func, if object is an array return it, otherwise wrap it with array

---
+++ test/Array/get-array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var getArgs = require('../../lib/Function/get-args');
+
+module.exports = function (t, a) {
+	var args = getArgs(1, 2), arr = [1,2,3];
+	a.deep(t(1), [1], "Primitive");
+	a.deep(t('str'), ['str'], "String");
+	a.deep(t(args), [args], "Arguments");
+	a(t(arr), arr, "Array");
+};
