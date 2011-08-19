2d46d2718be7316790f523c6dd929d32c02d3f41
Mariusz Nowak <medikoo@medikoo.com>
2011-08-19 21:14:37 +0200

    call binded to call function

---
+++ test/Function/call-call.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var k        = require('../../lib/Function/k')
+  , getScope = require('../../lib/Function/get-scope');
+
+module.exports = function (t, a) {
+	var x = {}, f = k(x);
+	a(t(f), x);
+	a(t(getScope, x), x, "Scope");
+};
