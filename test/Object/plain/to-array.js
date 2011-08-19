0f6cc5af5b2f9e135c4552e8b8fe63d9429efcbd
Mariusz Nowak <medikoo@medikoo.com>
2011-08-19 20:24:16 +0200

    Factory logic for Object's toArray

--- test/Object/plain/to-array.js
+++ test/Object/plain/to-array.js
@@ -2,14 +2,14 @@
 
 module.exports = function (t, a) {
 	var o = { foo: 'bar', raz: 'dwa' }, r, s;
-	t = t.call;
 
-	r = t(o);
+	r = t().call(o);
 	s = r.toString();
 	a(r.length, 2, "Multi length");
 	a.ok((s === 'foo,bar,raz,dwa') || (s === 'raz,dwa,foo,bar'), "Multi content");
 
-	r = t(o, true);
+	t = t(true).call;
+	r = t(o);
 	s = r.toString();
 	a(r.length, 4, "Flat length");
 	a.ok((s === 'foo,bar,raz,dwa') || (s === 'raz,dwa,foo,bar'), "Flat content");
