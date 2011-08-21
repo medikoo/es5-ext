1aa991da4185beb7ef0edbb7899567401d602894
Mariusz Nowak <medikoo@medikoo.com>
2011-08-21 20:32:23 +0200

    'intersection' for unlimited number of lists

--- test/List/intersection.js
+++ test/List/intersection.js
@@ -5,7 +5,7 @@ var toArray = require('../../lib/List/to-array').call;
 module.exports = {
 	__generic: function (t, a) {
 		t = t.call;
-		a.deep(t(this, this), toArray(this));
+		a.deep(t(this, this, this), toArray(this));
 	},
 	"": function (t, a) {
 		var x = {}, y = {}, p, r;
@@ -13,7 +13,8 @@ module.exports = {
 		a.deep(t([], [2,3,4]), [], "Empty #1");
 		a.deep(t([2,3,4], []), [], "Empty #2");
 		a.deep(t([2,3,x], [y,5,7]), [], "Different");
-		p = t([3, 5, 'raz', {}, 'dwa', x], [1, 3, 'raz', 'trzy', x, {}]);
+		p = t([3, 5, 'raz', {}, 'dwa', x], [1, 3, 'raz', 'dwa', 'trzy', x, {}],
+			[3, 'raz', x, 65]);
 		r = [3, 'raz', x];
 		p.sort(), r.sort();
 		a.deep(p, r, "Same parts");
