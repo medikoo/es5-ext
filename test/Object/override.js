daa6192399de2a89d32bff978f76fc546294ce40
Mariusz Nowak <medikoo@medikoo.com>
2011-08-27 16:54:41 +0200

    'override' for Objects, same logic as with 'extend' with that difference that doesn't extend object but operates on it

---
+++ test/Object/override.js
@@ -0,0 +1,18 @@
+'use strict';
+
+'use strict';
+
+module.exports = function (t, a)  {
+	t = t.call;
+	var o1 = { a: function (x) { return x + 1; }, b: function () { }, d: {} }
+	  , o2 = { a: { value: function (_super, x) { return _super(this, x); } },
+			c: { value: function () { } }, d: { value: {} } }
+
+	  , r = t(o1, o2);
+
+	a(r.a(2), 3, "Inheritance");
+	a(r.b, o1.b, "Ancestor methods");
+	a.ok(r.hasOwnProperty('b'), "Ancestor properties are own properties");
+	a(r.c, o2.c.value, "Descendant methods");
+	a(r.d, o2.d.value, "Non function property override");
+};
