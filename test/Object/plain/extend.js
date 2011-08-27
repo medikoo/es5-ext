519e0c5896e9655e715c0db608952c84681ddc78
Mariusz Nowak <medikoo@medikoo.com>
2011-08-27 16:08:56 +0200

    'extend' for plain objects (where we don't care about being descriptive)

---
+++ test/Object/plain/extend.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (t, a)  {
+	t = t.call;
+	var o1 = { a: function (x) { return x + 1; }, b: function () { }, d: {} }
+	  , o2 = { a: function (_super, x) { return _super(this, x); },
+			c: function () { }, d: {} }
+
+	  , r = t(o1, o2);
+
+	a(r.a(2), 3, "Inheritance");
+	a(r.b, o1.b, "Ancestor methods");
+	a.ok(!r.hasOwnProperty('b'), "Ancestor properties are not own properties");
+	a(r.c, o2.c, "Descendant methods");
+	a(r.d, o2.d, "Non function property override");
+};
