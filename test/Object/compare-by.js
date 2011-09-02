c20ac814d06948e1c1a17fbc89aac5f29f852cf8
Mariusz Nowak <medyk@medikoo.com>
2011-09-02 10:46:01 +0200

    All sort methods are now compare* methods in dedicated namespaces. Renamed Array/sort/byProperty to Object/compareBy.

--- test/Object/compare-by.js
+++ test/Object/compare-by.js
@@ -2,6 +2,7 @@
 
 module.exports = function (t, a) {
 	var x = { foo: 3 }, y = { foo: 1 }, z = { foo: 11 }, o = [x, y, z];
-	o.sort(t('foo'));
+	t = t('foo').call;
+	o.sort(t);
 	a.deep(o, [y, x, z]);
 };
