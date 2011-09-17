bfc6015c959487277b8319b43675bfa2dd6a12f2
Mariusz Nowak <medikoo@medikoo.com>
2011-09-17 11:09:18 +0200

    'applyArgs' addition to functionalize

--- test/Function/functionalize.js
+++ test/Function/functionalize.js
@@ -3,13 +3,15 @@
 var o = {};
 
 module.exports = function (t, a) {
-	var bind, call, apply, curry;
-	bind = t(function () { return this; }).bind;
-	a(bind(o)(), o, "Bind");
-	call = t(function () { return this; }).call;
-	a(call(a), a, "Call");
-	apply = t(function () { return this; }).apply;
-	a(apply(a), a, "Apply");
-	curry = t(function (a) { return this + a; }).curry;
-	a(curry('raz').call('dwa'), 'dwaraz', "Curry");
+	var fn
+	fn = t(function () { return this; }).bind;
+	a(fn(o)(), o, "Bind");
+	fn = t(function () { return this; }).call;
+	a(fn(a), a, "Call");
+	fn = t(function () { return this; }).apply;
+	a(fn(a), a, "Apply");
+	fn = t(function (a) { return this + a; }).curry;
+	a(fn('raz').call('dwa'), 'dwaraz', "Curry");
+	fn = t(function (a) { return this + a; }).call.applyArgs;
+	a(fn(['raz', 'dwa']), 'razdwa', "Apply args");
 };
