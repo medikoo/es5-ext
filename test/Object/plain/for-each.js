3c49aba342d32dc0d5e2adaa826ef73e167cd4ec
Mariusz Nowak <medikoo@medikoo.com>
2011-10-13 20:38:57 +0200

    Object forEach can now be processed in custom order. Compare function can now be passed as third argument. Additionally we may decide to run compare function on object keys instead of values.

--- test/Object/plain/for-each.js
+++ test/Object/plain/for-each.js
@@ -1,11 +1,37 @@
 'use strict';
 
 module.exports = function (t, a) {
-	var o = { 1: 1, 2: 2, 3: 3 }
-	  , o2 = {};
+	var o = { raz: 1, dwa: 2, trzy: 3 }
+	  , o2 = {}, o3 = {}, arr;
 	t = t.call;
-	t(o, function (value, name) {
+	t(o, function (value, name, self) {
 		o2[name] = value;
+		a(self, o, "Self");
+		a(this, o3, "Scope");
+	}, o3);
+	a.deep(o2, o);
+
+	arr = [], o2 = {};
+	t(o, function (value, name, self) {
+		arr.push(value);
+		o2[name] = value;
+		a(self, o, "Self");
+		a(this, o3, "Scope");
+	}, o3, function (a, b) {
+		return b - a;
 	});
-	a(JSON.stringify(o2), JSON.stringify(o));
+	a.deep(o2, o, "Sort by Values: Content");
+	a.deep(arr, [3, 2, 1], "Sort by Values: Order");
+
+	arr = [], o2 = {};
+	t(o, function (value, name, self) {
+		arr.push(value);
+		o2[name] = value;
+		a(self, o, "Self");
+		a(this, o3, "Scope");
+	}, o3, function (a, b) {
+		return a.localeCompare(b);
+	}, true);
+	a.deep(o2, o, "Sort by Keys: Content");
+	a.deep(arr, [2, 1, 3], "Sort by Keys: Order");
 };
