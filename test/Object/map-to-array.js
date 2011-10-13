7167d377fc905f57c9163aba2edcd2a39ccc677d
Mariusz Nowak <medikoo@medikoo.com>
2011-10-13 20:46:23 +0200

    Corrected third argument taken by callback passed to map2Array. Improved tests.

--- test/Object/map-to-array.js
+++ test/Object/map-to-array.js
@@ -2,9 +2,11 @@
 
 module.exports = function (t, a) {
 	t = t.call;
-	var o = { 1: 1, 2: 2, 3: 3 }
-	  , o2 = t(o, function (value, name) {
+	var o = { 1: 1, 2: 2, 3: 3 }, o1 = {}
+	  , o2 = t(o, function (value, name, self) {
+			a(self, o, "Self");
+			a(this, o1, "Scope");
 			return value + Number(name);
-		});
+		}, o1);
 	a.deep(o2, [2, 4, 6]);
 };
