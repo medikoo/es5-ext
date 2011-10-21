be900fb4e57d79d9edf508047126f47962b45d86
Damian <damian.sosnowski@roche.com>
2011-10-21 11:22:31 +0200

    Some fixes in map-right.js

--- test/List/map-right.js
+++ test/List/map-right.js
@@ -38,30 +38,21 @@ module.exports = {
 		a(last, 1, "Last is first");
 		a(result.length, 5, "Array has proper length");
 
-		result = t([1,2,3,4,5,6,7,8], function(element, index, arr){
-			if(element % 2 == 0)	{
-				return element;
-			}
-		});
-		a(result.length, 4, "Array has 4 elements");
-		a(result[0], 8, "First element is eq 8");
-		a(result[3], 2, "First element is eq 2")
-
 		result = t([1], function(element, index, arr){
 			return element + 4;
 		});
 		a(result[0], 5, "Array element is eq 5");
-//		t([1], function () { y = this; }, x);
-//		a(y, x, "Scope");
-//		y = 0;
-//		t([3,4,4], function (a, i) { y += i; });
-//		a(y, 3, "Indexes");
-//
-//		x = [1,3];
-//		x[5] = 'x';
-//		y = 0, count = 0;
-//		t(x, function (a, i) { ++count; y += i; });
-//		a(y, 6, "Misssing Indexes");
-//		a(count, 3, "Misssing Indexes, count");
+
+		start = new Array(10);
+		start[3] = 666;
+
+		result = t(start, function(element, index, arr){
+			return element/6;
+		});
+
+		a(Array.isArray(result), true, "Result is Array");
+		a(result.length, 10, "Array has correct length");
+		a(result[6], 111, "Array element has correct value");
+		a(result[0], undefined, "Empty array element is undefined");
 	}
 };
