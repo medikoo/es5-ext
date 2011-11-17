bf843eb9033903d8f5aab2c752c0dc3ff5e2fe13
Mariusz Nowak <medyk@medikoo.com>
2011-11-17 18:31:54 +0100

    Remove functionalization wrap for Array's "compact" and introduced two functions, one that takes target as scope (Array/prototype/compact.js) and other that takes it as first argument (Array/compact.js)

--- test/Array/prototype/compact.js
+++ test/Array/prototype/compact.js
@@ -3,6 +3,6 @@
 module.exports = function (t, a) {
 	var x = {}, y = [0, 1, "", null, NaN, x, false, true], z;
 
-	a(t(y), y, "Same object");
+	a(t.call(y), y, "Same object");
 	a.deep(y, [1, x, true], "Content");
 };
