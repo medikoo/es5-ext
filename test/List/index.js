6dfc632278806a606811b7ec0ca4111290f7073c
Mariusz Nowak <medyk@medikoo.com>
2011-11-17 18:13:26 +0100

    Do not add modules from prototype to main tree

---
+++ test/List/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var resolve   = require('path').resolve
+  , indexTest = require('tad/lib/utils/index-test')
+
+module.exports = indexTest(
+	indexTest.readDir(resolve(__dirname, '../../lib/List'))(function (o) {
+		delete o.prototype;
+		return o;
+	}), "index");
