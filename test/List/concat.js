b50c27eeef339ee57ddbbae30c8e6a255869bc43
Mariusz Nowak <medyk@medikoo.com>
2011-11-22 15:48:40 +0100

    Removed generic concat, it wasn't that helpful, and behind the scene it was actually introducing complexity instead of reducing it. We should do `toArray(list).concat(...)` instead

--- test/List/concat.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.__generic = function (t, a) {
-	t = t.call;
-	a(t(this, 4, 5).length, 5, "As context");
-	a(t(this, 4, this, 5).length, Array.isArray(this) ? 8 : 6, "As argument");
-};
