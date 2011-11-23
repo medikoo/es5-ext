2a0c5e8cd16a194028548f5170f0034f2b6202c9
Mariusz Nowak <medyk@medikoo.com>
2011-11-23 16:54:20 +0100

    Moved List's forEachRight into Array/prototype Removed functionalize wrap Fixed algorithm so it matches forEach by specification

--- test/Array/prototype/for-each-right.js
+++ test/Array/prototype/for-each-right.js
@@ -3,8 +3,7 @@
 module.exports = {
 	__generic: function (t, a) {
 		var count = 0, first, last, x, icount = this.length;
-		t = t.call;
-		t(this, function (item, index, col) {
+		t.call(this, function (item, index, col) {
 			++count;
 			if (!first) {
 				first = item;
@@ -16,21 +15,20 @@ module.exports = {
 		a(count, this.length, "Iterated");
 		a(first, this[this.length - 1], "First is last");
 		a(last, this[0], "Last is first");
-		a(x, this, "Collection as third argument");
+		a.deep(x, Object(this), "Collection as third argument");
 	},
 	"": function (t, a) {
 		var x = {}, y, count;
-		t = t.call;
-		t([1], function () { y = this; }, x);
+		t.call([1], function () { y = this; }, x);
 		a(y, x, "Scope");
 		y = 0;
-		t([3,4,4], function (a, i) { y += i; });
+		t.call([3,4,4], function (a, i) { y += i; });
 		a(y, 3, "Indexes");
 
 		x = [1,3];
 		x[5] = 'x';
 		y = 0, count = 0;
-		t(x, function (a, i) { ++count; y += i; });
+		t.call(x, function (a, i) { ++count; y += i; });
 		a(y, 6, "Misssing Indexes");
 		a(count, 3, "Misssing Indexes, count");
 	}
