44aa33722599a9d7296b0b75fdf941c1b3ea2ca9
Mariusz Nowak <medyk@medikoo.com>
2011-11-23 16:22:29 +0100

    Moved List's flatten into Array/prototype Flatten should always return an array (do not return same string if string is given) Remove functionalize wrap

--- test/Array/prototype/flatten.js
+++ test/Array/prototype/flatten.js
@@ -4,11 +4,9 @@ var o = [1, 2, [3, 4, [5, 6], 7, 8], 9, 10];
 
 module.exports = {
 	__generic : function (t, a) {
-		t = t.call;
-		a(t(this).length, 3);
+		a(t.call(this).length, 3);
 	},
 	"Nested Arrays": function (t, a) {
-		t = t.call;
-		a(t(o).length, 10);
+		a(t.call(o).length, 10);
 	}
 };
