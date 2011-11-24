d4feadef7b1fc039501ff982ee8006f26291639d
Mariusz Nowak <medyk@medikoo.com>
2011-11-24 13:49:14 +0100

    Moved List's 'someRight' into Array/prototype Removed functionalize wrap Algorithm for 'someRight' now matches more closely 'some' algorithm (as specified)

--- test/Array/prototype/some-right.js
+++ test/Array/prototype/some-right.js
@@ -3,8 +3,7 @@
 module.exports = {
 	__generic: function (t, a) {
 		var count = 0, first, last, x, icount = this.length;
-		t = t.call;
-		t(this, function (item, index, col) {
+		t.call(this, function (item, index, col) {
 			++count;
 			if (!first) {
 				first = item;
@@ -16,26 +15,25 @@ module.exports = {
 		a(count, this.length, "Iterated");
 		a(first, this[this.length - 1], "First is last");
 		a(last, this[0], "Last is first");
-		a(x, this, "Collection as third argument");
+		a.deep(x, Object(this), "Collection as third argument");
 	},
 	"": function (t, a) {
 		var x = {}, y, count;
-		t = t.call;
-		t([1], function () { y = this; }, x);
+		t.call([1], function () { y = this; }, x);
 		a(y, x, "Scope");
 		y = 0;
-		t([3,4,4], function (a, i) { y += i; });
+		t.call([3,4,4], function (a, i) { y += i; });
 		a(y, 3, "Indexes");
 
 		x = [1,3];
 		x[5] = 'x';
 		y = 0, count = 0;
-		a(t(x, function (a, i) { ++count; y += i; }), false, "Return");
+		a(t.call(x, function (a, i) { ++count; y += i; }), false, "Return");
 		a(y, 6, "Misssing Indexes");
 		a(count, 3, "Misssing Indexes, count");
 
 		count = 0;
-		a(t([-2,-3,-4, 2, -5], function (item) {
+		a(t.call([-2,-3,-4, 2, -5], function (item) {
 			++count;
 			return item > 0;
 		}), true, "Return");
