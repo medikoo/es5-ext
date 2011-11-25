ed419f384e17742c1554c2f72fe61d02986c168e
Mariusz Nowak <medyk@medikoo.com>
2011-11-25 16:16:17 +0100

    Renamed Date/format into Date/get-format as it's format function factory. Removed functionalize wrap.

--- test/Date/get-format.js
+++ test/Date/get-format.js
@@ -2,6 +2,6 @@
 
 module.exports = function (t, a) {
 	var dt = new Date(2011, 2, 3, 3, 5, 5, 32);
-	t = t(' %Y.%y.%m.%d.%H.%M.%S.%L ').call;
-	a(t(dt), ' 2011.11.03.03.03.05.05.032 ');
+	t = t(' %Y.%y.%m.%d.%H.%M.%S.%L ');
+	a(t.call(dt), ' 2011.11.03.03.03.05.05.032 ');
 };
