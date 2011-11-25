632e674773fe0987a3248f69feeafa8aefa70fab
Mariusz Nowak <medyk@medikoo.com>
2011-11-25 14:25:02 +0100

    Moved Date's day/floor into prototype/day-floor. Removed functionalize wrap

--- test/Date/prototype/day-floor.js
+++ test/Date/prototype/day-floor.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function (t, a) {
-	t = t.call;
-	a(t(new Date(2000, 0, 1, 13, 32, 34, 234)).valueOf(),
+	a(t.call(new Date(2000, 0, 1, 13, 32, 34, 234)).valueOf(),
 		new Date(2000, 0, 1).valueOf());
 };
