181b49a8128b069d775a1572280c2be6c3994abe
Mariusz Nowak <medyk@medikoo.com>
2011-11-25 12:55:31 +0100

    Revised logic of binaryBoundarySearch. Made it as more generic simple binary search function. Moved it out from 'sorted' folder and placed it as object method in Array/prototype. It's named bsearchClosest now

---
+++ test/Array/prototype/bsearch-closest.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var compare  = require('../../../lib/Number/prototype/compare');
+
+module.exports = function (t, a) {
+	var arr, fn, search;
+	arr = [2,5,5,8,34,67,98,345,678];
+
+	// highest, equal match
+	a(t.call(arr, compare.bind(1)), -1, "All higher");
+	a(t.call(arr, compare.bind(679)), arr.length - 1, "All lower");
+	a(t.call(arr, compare.bind(4)), 0, "Mid");
+	a(t.call(arr, compare.bind(5)), 2, "Match");
+	a(t.call(arr, compare.bind(6)), 2, "Above");
+};
