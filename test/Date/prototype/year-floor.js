9b837a4fb2eeb23bc6cb536a81a3f343b7859543
Mariusz Nowak <medyk@medikoo.com>
2011-11-25 16:45:29 +0100

    Moved Date/year/floor into Date/prototype/year-floor. Removed functionalize wrap.

--- test/Date/prototype/year-floor.js
+++ test/Date/prototype/year-floor.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = function (t, a) {
-	t = t.call;
-	a(t(new Date(2000, 5, 13, 13, 32, 34, 234)).valueOf(),
+	a(t.call(new Date(2000, 5, 13, 13, 32, 34, 234)).valueOf(),
 		new Date(2000, 0, 1).valueOf());
 };
 
