9e95d1fd6da7455bac8869ad3afabe5da57d310f
Mariusz Nowak <mariusz@medikoo.com>
2011-11-26 20:20:41 +0100

    Renamed Funtion's sequence into chain and moved it into Function/prototype. Removed functionalize wrap.

--- test/Function/prototype/chain.js
+++ test/Function/prototype/chain.js
@@ -13,7 +13,5 @@ h = function (a) {
 };
 
 module.exports = function (t, a) {
-	t = t.call;
-	a(t(f, g, h)(1, 2).toString(),
-		['c', 1, 'b', 1, 'a', 2, 1, 2].toString());
+	a.deep(t.call(f, g, h)(1, 2), ['c', 1, 'b', 1, 'a', 2, 1, 2]);
 };
