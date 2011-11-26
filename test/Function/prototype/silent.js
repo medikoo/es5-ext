524ddb81cc033434aded364f164a3ba4221400ee
Mariusz Nowak <mariusz@medikoo.com>
2011-11-26 20:24:32 +0100

    Moved Fuction's silent into Function/prototype. Removed functionalize wrap.

--- test/Function/prototype/silent.js
+++ test/Function/prototype/silent.js
@@ -3,8 +3,7 @@
 module.exports = function (t, a) {
 	var e = new Error('Error'), x = {}, f1 = function (y) { return y; }
 	  , f2 = function () { throw e; };
-	t = t.call;
 
-	a(t(f1, x), x);
-	a(t(f2), e, "Thrown");
+	a(t.call(f1, x), x);
+	a(t.call(f2), e, "Thrown");
 };
