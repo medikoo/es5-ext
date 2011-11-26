cea4554234e0f127c588242c16fd25aa0f87cc24
Mariusz Nowak <mariusz@medikoo.com>
2011-11-26 20:12:03 +0100

    Moved Fuction's rncurry into Function/prototype. Removed functionalize wrap.

---
+++ test/Function/prototype/rncurry.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var toArray = require('../../../lib/Array/to-array');
+
+var f = function () {
+	return toArray(arguments);
+};
+
+module.exports = function (t, a) {
+	a.deep(t.call(f, 0, 1, 2)(), [], "0 arguments");
+	a.deep(t.call(f, 5, 1, 2)(3, 4)(5, 6), [5, 3, 4, 1, 2], "Many arguments");
+};
