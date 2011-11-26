2deb55ccd838bc6fb963d6d93ab7010999bb5abb
Mariusz Nowak <mariusz@medikoo.com>
2011-11-26 17:30:38 +0100

    Moved Function's aritize to Function/prototype. Removed functionalize wrap.

--- test/Function/prototype/aritize.js
+++ test/Function/prototype/aritize.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var toArray = require('../../lib/Array/to-array');
+var toArray = require('../../../lib/Array/to-array');
 
 module.exports = function (t, a) {
-	t = t.call;
-	a(t(function (a, b, c) {
+	a(t.call(function (a, b, c) {
 		return toArray(arguments).toString() + '|' + String(arguments.length);
 	}, 2)(1, 2, 3), '1,2|2');
 };
