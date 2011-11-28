ccd7a84d79a0f2bae7d9b3648e7b3c664f355f7d
Mariusz Nowak <mariusz@medikoo.com>
2011-11-28 08:56:09 +0100

    Renamed Object's compareBy into getCompareBy. Removed functionalize wrap.

--- test/Object/get-compare-by.js
+++ test/Object/get-compare-by.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var call = Function.prototype.call;
+
 module.exports = function (t, a) {
 	var x = { foo: 3 }, y = { foo: 1 }, z = { foo: 11 }, o = [x, y, z];
-	t = t('foo').call;
+	t = call.bind(t('foo'));
 	o.sort(t);
 	a.deep(o, [y, x, z]);
 };
