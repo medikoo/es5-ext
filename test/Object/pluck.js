548316b5c34b27a4fd21b41725fa5256309bad77
Mariusz Nowak <medyk@medikoo.com>
2011-11-29 15:37:38 +0100

    Removed Object's pluck. There seems to be no use case for that.

--- test/Object/pluck.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var same = require('../../lib/Object/plain/same').call;
-
-module.exports = function (t, a) {
-	var o = { a: 'b' };
-	t = t.call;
-	a.ok(same(t(o, 'a'), Object.getOwnPropertyDescriptor(o, 'a')));
-};
