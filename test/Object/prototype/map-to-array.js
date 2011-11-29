e2464547c437d11ac50e551155f06839d5cdbbb3
Mariusz Nowak <medyk@medikoo.com>
2011-11-29 15:07:43 +0100

    Moved Object's mapToArray into Object/prototype. Removed functionalize wrap.

--- test/Object/prototype/map-to-array.js
+++ test/Object/prototype/map-to-array.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = function (t, a) {
-	t = t.call;
 	var o = { 1: 1, 2: 2, 3: 3 }, o1 = {}
-	  , o2 = t(o, function (value, name, self) {
+	  , o2 = t.call(o, function (value, name, self) {
 			a(self, o, "Self");
 			a(this, o1, "Scope");
 			return value + Number(name);
