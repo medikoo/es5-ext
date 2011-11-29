6c75e555f61d90b88b933f591e287e116a483182
Mariusz Nowak <medyk@medikoo.com>
2011-11-29 17:42:52 +0100

    Moved Object's delete from 'plain' into 'prototype' folder. Renamed it to 'unset'. Removed functionalize wrap.

--- test/Object/prototype/unset.js
+++ test/Object/prototype/unset.js
@@ -2,9 +2,8 @@
 
 module.exports = function (t, a) {
 	var x = { foo: 'bar', bar: 34 };
-	t = t.call;
 
-	t(x, 'foo');
+	t.call(x, 'foo');
 	a(x.hasOwnProperty('foo'), false, "Remove named property");
 	a(x.bar, 34, "Leave other properties intact");
 };
