8b23bfaea898954bf340747d35477aaf30132718
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 16:16:11 +0100

    Moved Object's extend from 'plain' into 'prototype' folder and named it plainExtend. Removed functionalize wrap.

--- test/Object/prototype/plain-extend.js
+++ test/Object/prototype/plain-extend.js
@@ -1,12 +1,11 @@
 'use strict';
 
 module.exports = function (t, a)  {
-	t = t.call;
 	var o1 = { a: function (x) { return x + 1; }, b: function () { }, d: {} }
 	  , o2 = { a: function (_super, x) { return _super(this, x); },
 			c: function () { }, d: {} }
 
-	  , r = t(o1, o2);
+	  , r = t.call(o1, o2);
 
 	a(r.a(2), 3, "Inheritance");
 	a(r.b, o1.b, "Ancestor methods");
