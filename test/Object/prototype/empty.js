ba98c8518c798e51b3c8e25f2532c44c63d344c7
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 15:37:04 +0100

    Moved Object's empty from 'plain' into 'prototype' folder. Removed functionalize wrap.

--- test/Object/prototype/empty.js
+++ test/Object/prototype/empty.js
@@ -4,10 +4,9 @@ var isEmpty = require('../../../lib/Object/plain/is-empty').call;
 
 module.exports = function (t, a) {
 	var x = {};
-	t = t.call;
-	a(t(x), x, "Empty: Returns same object");
+	a(t.call(x), x, "Empty: Returns same object");
 	a(isEmpty(x), true, "Empty: Not changed");
 	x.foo = 'raz', x.bar = 'dwa';
-	a(t(x), x, "Same object");
+	a(t.call(x), x, "Same object");
 	a(isEmpty(x), true, "Emptied");
 };
