d87c045b24c8b64dfe9471604aac385bb6d6996c
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 16:20:55 +0100

    Moved Object's filter from 'plain' into 'prototype' folder Removed functionalize wrap.

--- test/Object/plain/filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	t = t.call;
-	a(JSON.stringify(t({ 1: 1, 2: 2, 3: 3, 4: 4 },
-		function (value, key) { return !!(value%2); }))
-		  , JSON.stringify({ 1: 1, 3: 3 }));
-};
