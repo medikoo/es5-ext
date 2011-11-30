aab3b6ba949627311a23726133c45a8e5d357b51
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 15:35:00 +0100

    Moved Object's elevate from 'plain' into 'prototype' folder. Renamed elevate into flatten and made it work analogously as Array's flatten. Removed functionalize wrap.

--- test/Object/plain/elevate.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	t = t.call;
-	var o = t({ a: { aa: 1, ab: 2 }, b: { ba: 3, bb: 4 } });
-	a([o.aa, o.ab, o.ba, o.bb].toString(), [1,2,3,4].toString());
-};
