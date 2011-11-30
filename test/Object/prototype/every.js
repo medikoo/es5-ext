a5431fa30f98e06bb35b97711d8650625a07a623
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 15:41:03 +0100

    Moved Object's every (and it's '_iterate' base function) from 'plain' into 'prototype' folder. Removed functionalize wrap.

--- test/Object/prototype/every.js
+++ test/Object/prototype/every.js
@@ -4,18 +4,17 @@ var o = { 1: 1, 2: 2, 3: 3 };
 
 module.exports = function (t, a) {
 	var o2 = {};
-	t = t.call;
-	t(o, function (value, name) {
+	t.call(o, function (value, name) {
 		o2[name] = value;
 		return true;
 	});
 	a(JSON.stringify(o2), JSON.stringify(o), "Iterates");
 
-	a(t(o, function () {
+	a(t.call(o, function () {
 		return true;
 	}), true, "Succeeds");
 
-	a(t(o, function () {
+	a(t.call(o, function () {
 		return false;
 	}), false, "Fails");
 
