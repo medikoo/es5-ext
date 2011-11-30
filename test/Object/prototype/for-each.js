7e4bc21695df72bd62e6c62e5cccc6a6a8bd3e4c
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 16:29:33 +0100

    Moved Object's forEach from 'plain' into 'prototype' folder Removed functionalize wrap.

--- test/Object/prototype/for-each.js
+++ test/Object/prototype/for-each.js
@@ -3,8 +3,7 @@
 module.exports = function (t, a) {
 	var o = { raz: 1, dwa: 2, trzy: 3 }
 	  , o2 = {};
-	t = t.call;
-	a(t(o, function (value, name) {
+	a(t.call(o, function (value, name) {
 		o2[name] = value;
 	}), undefined, "Return");
 	a.deep(o2, o);
