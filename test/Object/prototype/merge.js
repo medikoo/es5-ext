190f2b011032fa7ff8ca4ced54a86a347bbb8fc4
Mariusz Nowak <medyk@medikoo.com>
2011-11-30 17:38:44 +0100

    Moved Object's merge from 'plain' into 'prototype' folder Removed functionalize wrap.

--- test/Object/prototype/merge.js
+++ test/Object/prototype/merge.js
@@ -4,8 +4,7 @@ module.exports = function (t, a) {
 	var o1 = { a: 1, b: 2 }
 	  , o2 = { b: 3, c: 4 };
 
-	t = t.call;
-	t(o1, o2);
+	t.call(o1, o2);
 
 	a(o1.a, 1, "Keep");
 	a(o1.b, 3, "Overwrite");
