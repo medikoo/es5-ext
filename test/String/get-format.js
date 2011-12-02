5a6d07bcd74b473b0af73b5c5a17117daa2bf5aa
Mariusz Nowak <mariusz@medikoo.com>
2011-12-02 18:47:29 +0100

    Renamed String's 'format' into 'get-format'. Removed functionalize wrap. Made it's result function generic.

--- test/String/get-format.js
+++ test/String/get-format.js
@@ -3,7 +3,7 @@
 module.exports = function (t, a) {
 	var o = { a: 0 };
 	t = t({ a: 'A', aa: 'B', ab: 'C', b: 'D',
-		c: function () { return ++this.a; } }).call;
+		c: function () { return ++this.a; } });
 
-	a(t(' %a%aab%abb%b\\%aa%ab%c%c ', o), ' ABbCbD%aaC12 ');
+	a(t.call(' %a%aab%abb%b\\%aa%ab%c%c ', o), ' ABbCbD%aaC12 ');
 };
