e4f611e6acdd9ffb7b150f97558e9d17a7fbb186
Mariusz Nowak <medyk@medikoo.com>
2011-12-06 13:32:37 +0100

    Moved String's repeat into 'prototype' folder. Removed functionalize wrap.

--- test/String/repeat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (t, a) {
-	t = t.call;
-	a(t('a', 0), '', "Empty");
-	a(t('a', 1), 'a', "1");
-	a(t('\t', 5), '\t\t\t\t\t', "Whitespace");
-	a(t('raz', 3), 'razrazraz', "Many chars");
-};
