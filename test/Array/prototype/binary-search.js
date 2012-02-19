ef84afc5390bd7fdcf3c3cf743b106eb2e68c4c0
Mariusz Nowak <mariusz@medikoo.com>
2012-02-19 18:13:11 +0100

    Fixed Array's binarySearch test

--- test/Array/prototype/binary-search.js
+++ test/Array/prototype/binary-search.js
@@ -7,7 +7,7 @@ module.exports = function (t, a) {
 	arr = [2, 5, 5, 8, 34, 67, 98, 345, 678];
 
 	// highest, equal match
-	a(t.call(arr, compare.bind(1)), -1, "All higher");
+	a(t.call(arr, compare.bind(1)), 0, "All higher");
 	a(t.call(arr, compare.bind(679)), arr.length - 1, "All lower");
 	a(t.call(arr, compare.bind(4)), 0, "Mid");
 	a(t.call(arr, compare.bind(5)), 2, "Match");
