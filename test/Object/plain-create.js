c25ead62847cb7dabcecf78dd570c0f8e4fcb7cf
Mariusz Nowak <mariusz@medikoo.com>
2012-03-28 21:48:58 +0200

    Moved Object.prototype.plainCreate to Object.plainCreate

--- test/Object/plain-create.js
+++ test/Object/plain-create.js
@@ -2,12 +2,12 @@
 
 module.exports = function (t, a) {
 	var x = {}, y = { foo: 'bar' }, o;
-	o = t.call(x, y);
+	o = t(x, y);
 
 	a(o.foo, 'bar', "Property");
 	a(Object.getPrototypeOf(o), x, "Prototype");
 	a('foo' in x, false, "Foo on top of prototype chain"); //jslint: skip
 
-	o = t.call(x);
+	o = t(x);
 	a(Object.getPrototypeOf(o), x, "Prototype (no properties)");
 };
