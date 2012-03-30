5b7ce2a7fc706a8b086983fc5e660993b47e8164
Mariusz Nowak <mariusz@medikoo.com>
2012-03-30 16:43:48 +0200

    Stricter isEqual

--- test/is-equal.js
+++ test/is-equal.js
@@ -6,4 +6,7 @@ module.exports = function (t, a) {
 	a(t(o, o), true, "Same objects");
 	a(t('1', '1'), true, "Same primitive");
 	a(t('1', 1), false, "Different primitive types");
+	a(t(NaN, NaN), true, "NaN");
+	a(t(0, 0), true, "0,0");
+	a(t(0, -0), false, "0,-0");
 };
