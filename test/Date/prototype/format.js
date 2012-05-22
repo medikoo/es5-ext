0785fb6286e3a0b504c0c4e75212780320464008
Mariusz Nowak <mariusz@medikoo.com>
2012-05-22 22:58:55 +0200

    Turn Date's getFormat into format method
    getFormat factory don't make much sense when we can achieve same thing
    with help of function's partial. `format` is now regular method for
    Date instances.

--- test/Date/prototype/format.js
+++ test/Date/prototype/format.js
@@ -2,6 +2,5 @@
 
 module.exports = function (t, a) {
 	var dt = new Date(2011, 2, 3, 3, 5, 5, 32);
-	t = t(' %Y.%y.%m.%d.%H.%M.%S.%L ');
-	a(t.call(dt), ' 2011.11.03.03.03.05.05.032 ');
+	a(t.call(dt, ' %Y.%y.%m.%d.%H.%M.%S.%L '), ' 2011.11.03.03.03.05.05.032 ');
 };
