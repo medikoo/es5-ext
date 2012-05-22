93dd96997f93cced78f72bae3d12903389468f94
Mariusz Nowak <mariusz@medikoo.com>
2012-05-22 14:38:53 +0200

    Better tests for Array.generate

--- test/Array/generate.js
+++ test/Array/generate.js
@@ -2,6 +2,8 @@
 
 module.exports = function (t, a) {
 	var x = {}, y = {};
+	a.deep(t(), [], "No arguments");
+	a.deep(t(3), [undefined, undefined, undefined], "Just length");
 	a.deep(t(0, 'x'), [], "No repeat");
 	a.deep(t(1, x, y), [x], "Arguments length larger than repeat number");
 	a.deep(t(3, x), [x, x, x], "Single argument");
