98aaa98e41c79e73286553521140a910982ba9e2
Mariusz Nowak <mariusz@medikoo.com>
2012-05-23 13:01:07 +0200

    Removed Function.prototype.aritize
    It's use case is well handled by `Function.prototype.curry` now.

--- test/Function/prototype/aritize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var toArray = require('../../../lib/Object/to-array');
-
-module.exports = function (t, a) {
-	a(t.call(function (a, b, c) {
-		return toArray(arguments).toString() + '|' + String(arguments.length);
-	}, 2)(1, 2, 3), '1,2|2');
-};
