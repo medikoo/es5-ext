11c5652ec1505d9b2dd3c26e1136c739aa11a3ec
Mariusz Nowak <mariusz@medikoo.com>
2012-05-24 12:45:18 +0200

    Reserved keywords is now list not hash
    It's probably more natural approach

--- test/reserved.js
+++ test/reserved.js
@@ -1,25 +1,30 @@
 'use strict';
 
+var contains = require('../lib/Array/prototype/contains');
+
 module.exports = function (t, a) {
-	a(t.keywords['break'], true, "Keywords hash has reserved keyword");
-	a(t.keywords['class'], undefined,
+	a(contains.call(t.keywords, 'break'), true,
+		"Keywords hash has reserved keyword");
+	a(contains.call(t.keywords, 'class'), false,
 		"Keywords hash has not future reserved word");
-	a(t.keywords['let'], undefined,
+	a(contains.call(t.keywords, 'let'), false,
 		"Keywords hash has not future strict reserved word");
-	a(t.future['break'], undefined,
+	a(contains.call(t.future, 'break'), false,
 		"Future reserved hash has not reserved keyword");
-	a(t.future['class'], true, "Future reserved hash has future reserved word");
-	a(t.future['let'], undefined,
+	a(contains.call(t.future, 'class'), true,
+		"Future reserved hash has future reserved word");
+	a(contains.call(t.future, 'let'), false,
 		"Future reserved hash has not future strict reserved word");
-	a(t.futureStrict['break'], undefined,
+	a(contains.call(t.futureStrict, 'break'), false,
 		"Future strict reserved hash has not reserved keyword");
-	a(t.futureStrict['class'], undefined,
+	a(contains.call(t.futureStrict, 'class'), false,
 		"Future strict reserved hash has not future reserved word");
-	a(t.futureStrict['let'], true,
+	a(contains.call(t.futureStrict, 'let'), true,
 		"Future reserved hash has future strict reserved word");
-	a(t.all['break'], true, "All reserved keywords hash has reserved keyword");
-	a(t.all['class'], true,
+	a(contains.call(t, 'break'), true,
+		"All reserved keywords hash has reserved keyword");
+	a(contains.call(t, 'class'), true,
 		"All reserved keywords hash has future reserved word");
-	a(t.all['let'], true,
+	a(contains.call(t, 'let'), true,
 		"All reserved keywords hash has future strict reserved word");
 };
