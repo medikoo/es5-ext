3123fdeb12a1692ddeb284646da4f43ce58a745f
Mariusz Nowak <mariusz@medikoo.com>
2012-07-13 13:23:20 +0200

    Fix Function.prototype.curry
    With latest improvement we exposed change of state to different
    invocations (dumb). Added test for that case.

--- test/Function/prototype/curry.js
+++ test/Function/prototype/curry.js
@@ -7,6 +7,9 @@ var f = function () {
 };
 
 module.exports = function (t, a) {
+	var x, y = {};
 	a.deep(t.call(f, 0, 1, 2)(3), [], "0 arguments");
-	a.deep(t.call(f, 5, {})(1, 2)(3, 4)(5, 6), [1, 2, 3, 4, 5], "Many arguments");
+	x = t.call(f, 5, {});
+	a.deep(x(1, 2)(3, 4)(5, 6), [1, 2, 3, 4, 5], "Many arguments");
+	a.deep(x(8, 3)(y, 45)('raz', 6), [8, 3, y, 45, 'raz'], "Many arguments #2");
 };
