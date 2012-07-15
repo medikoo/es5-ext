8837518668744061154bde7513bd072ebb9b8592
Mariusz Nowak <mariusz@medikoo.com>
2012-07-15 18:51:01 +0200

    Fix clear cache for primitive mode

--- test/Function/prototype/memoize.js
+++ test/Function/prototype/memoize.js
@@ -260,6 +260,18 @@ module.exports = function (t, a) {
 				a(mfn(y, 'bar', 'zeta'), 'foobarzeta', "#1");
 				a(mfn('foo', 'bar', 'zeta'), 'foobarzeta', "#2");
 				a(i, 1, "Called once");
+			},
+			"Clear cache": function (a) {
+				var i = 0, fn = function (x, y, z) { ++i; return x + y + z; }, mfn
+				  , y = { toString: function () { return 'foo' } };
+				mfn = t.call(fn, { primitive: true });
+				a(mfn(y, 'bar', 'zeta'), 'foobarzeta', "#1");
+				a(mfn('foo', 'bar', 'zeta'), 'foobarzeta', "#2");
+				a(i, 1, "Called once");
+				mfn.clearCache('foo', { toString: function () { return 'bar'; } },
+					'zeta');
+				a(mfn(y, 'bar', 'zeta'), 'foobarzeta', "#3");
+				a(i, 2, "Called twice");
 			}
 		}
 	};
