ca933bdb795eab527af78df549fd76ffd0c66e48
Mariusz Nowak <mariusz@medikoo.com>
2013-08-23 10:40:01 +0200

    Fix Array#eIndexOf

--- test/Array/prototype/e-index-of.js
+++ test/Array/prototype/e-index-of.js
@@ -7,4 +7,7 @@ module.exports = function (t, a) {
 	a(t.call([3, 'raz', 0, {}, -0], -0), 4, "-0");
 	a(t.call([3, 'raz', -0, {}, 0], +0), 4, "+0");
 	a(t.call([3, 'raz', NaN, {}, NaN], NaN, 3), 4, "fromIndex");
+	a(t.call([3, 'raz', NaN, {}, NaN], NaN, -1), 4, "fromIndex negative #1");
+	a(t.call([3, 'raz', NaN, {}, NaN], NaN, -2), 4, "fromIndex negative #2");
+	a(t.call([3, 'raz', NaN, {}, NaN], NaN, -3), 2, "fromIndex negative #3");
 };
