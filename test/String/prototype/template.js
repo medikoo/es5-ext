39ed1f7d538b6491658320425e3e046f18a1f248
Mariusz Nowak <mariusz@medikoo.com>
2013-08-26 20:30:38 +0200

    Improve template algorithm

--- test/String/prototype/template.js
+++ test/String/prototype/template.js
@@ -4,14 +4,12 @@ module.exports = function (t, a) {
 	var compiled;
 
 	compiled = t.call('%raz% \\%\\% f%prik%oo %maroko%\n\\%mis\\1k\\2o%markas%%' +
-		'moled%ech%%eloo%su%', {
-			raz: true,
+		'moled%ech%%eloo%su%elo', {
 			prik: function (context) { return context.prik2; },
-			markas: function (context) { return context.foo; },
-			moled: true,
-			su: true
+			markas: function (context) { return context.foo; }
 		});
 
 	a(compiled({ raz: 'raz1', prik2: 23, foo: 'morda', moled: 'eho',
-		su: 'vivi' }), 'raz1 %% f23oo %maroko%\n%mis\\1k\\2omordaehoech%%eloovivi');
+		su: 'vivi' }), 'raz1 %% f23oo \n%mis\\1k\\2omordaehoechelooviv' +
+		'ielo');
 };
