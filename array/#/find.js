416538330ef5e93241a9fe3a79d60dd355a28f63
Mariusz Nowak <mariusz@medikoo.com>
2013-10-01 19:24:52 +0200

    Reuse findIndex logic within find

--- array/#/find.js
+++ array/#/find.js
@@ -1,20 +1,8 @@
 'use strict';
 
-var callable = require('../../object/valid-callable')
-  , value    = require('../../object/valid-value')
-
-  , some = Array.prototype.some, apply = Function.prototype.apply;
+var findIndex = require('./find-index');
 
 module.exports = function (predicate/*, thisArg*/) {
-	var kValue, self;
-	self = Object(value(this));
-	callable(predicate);
-
-	return some.call(self, function (value) {
-		if (apply.call(predicate, this, arguments)) {
-			kValue = value;
-			return true;
-		}
-		return false;
-	}, arguments[1]) ? kValue : undefined;
+	var index = findIndex.apply(this, arguments);
+	return (index === -1) ? undefined : this[index];
 };
