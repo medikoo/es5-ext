f4e4c3e807c0aebdececa150389e0ea323522f46
Mariusz Nowak <mariusz@medikoo.com>
2013-10-03 14:06:43 +0200

    Algorithm cleanup

--- array/#/exclusion.js
+++ array/#/exclusion.js
@@ -10,20 +10,18 @@ var value    = require('../../object/valid-value')
 
 module.exports = function (/*…lists*/) {
 	var lists, seen, result;
-	if (arguments.length) {
-		push.apply(lists = [this], arguments);
-		lists.forEach(value);
-		seen = [];
-		result = [];
-		lists.sort(byLength).forEach(function (list) {
-			result = result.filter(function (item) {
-				return !contains.call(list, item);
-			}).concat(filter.call(list, function (x) {
-				return !contains.call(seen, x);
-			}));
-			push.apply(seen, toArray(list));
-		});
-		return result;
-	}
-	return aFrom(this);
+	if (!arguments.length) return aFrom(this);
+	push.apply(lists = [this], arguments);
+	lists.forEach(value);
+	seen = [];
+	result = [];
+	lists.sort(byLength).forEach(function (list) {
+		result = result.filter(function (item) {
+			return !contains.call(list, item);
+		}).concat(filter.call(list, function (x) {
+			return !contains.call(seen, x);
+		}));
+		push.apply(seen, toArray(list));
+	});
+	return result;
 };
