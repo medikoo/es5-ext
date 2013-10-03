83d2d758072558d709f14de4e320fb2c5242dff0
Mariusz Nowak <mariusz@medikoo.com>
2013-10-03 14:05:35 +0200

    Simplify logic of eIndexOf and eLastIndexOf

--- array/#/e-index-of.js
+++ array/#/e-index-of.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var numIsNaN = require('../../number/is-nan')
-  , eq       = require('../../object/eq')
-  , value    = require('../../object/valid-value')
+var value    = require('../../object/valid-value')
 
   , indexOf = Array.prototype.indexOf
   , hasOwnProperty = Object.prototype.hasOwnProperty
   , abs = Math.abs, floor = Math.floor;
 
 module.exports = function (searchElement/*, fromIndex*/) {
-	var i, l, fromIndex;
-	if (searchElement || (!numIsNaN(searchElement) && (searchElement !== 0))) {
+	var i, l, fromIndex, val;
+	if (searchElement === searchElement) { //jslint: skip
 		return indexOf.apply(this, arguments);
 	}
 
@@ -21,7 +19,10 @@ module.exports = function (searchElement/*, fromIndex*/) {
 	else fromIndex = (this.length >>> 0) - floor(abs(fromIndex));
 
 	for (i = fromIndex; i < l; ++i) {
-		if (hasOwnProperty.call(this, i) && eq(searchElement, this[i])) return i;
+		if (hasOwnProperty.call(this, i)) {
+			val = this[i];
+			if (val !== val) return i; //jslint: skip
+		}
 	}
 	return -1;
 };
