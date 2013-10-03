60f4f86061165a5e58a27c7d5da03628954fce6c
Mariusz Nowak <mariusz@medikoo.com>
2013-10-03 14:15:38 +0200

    -- whitespace

--- array/#/last.js
+++ array/#/last.js
@@ -4,8 +4,6 @@ var lastIndex = require('./last-index');
 
 module.exports = function () {
 	var i;
-	if ((i = lastIndex.call(this)) !== null) {
-		return this[i];
-	}
+	if ((i = lastIndex.call(this)) !== null) return this[i];
 	return undefined;
 };
