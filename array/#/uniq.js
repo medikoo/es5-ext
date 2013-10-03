c31f15910183bc84898591e8ffe2295e62c8fded
Mariusz Nowak <mariusz@medikoo.com>
2013-10-03 14:32:51 +0200

    -- whitespace

--- array/#/uniq.js
+++ array/#/uniq.js
@@ -10,6 +10,4 @@ isFirst = function (value, index) {
 	return indexOf.call(this, value) === index;
 };
 
-module.exports = function () {
-	return filter.call(this, isFirst, this);
-};
+module.exports = function () { return filter.call(this, isFirst, this); };
