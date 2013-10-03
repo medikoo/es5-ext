30cca1530dd97ce22bd7bd06503c493a9169d48a
Mariusz Nowak <mariusz@medikoo.com>
2013-10-03 14:12:58 +0200

    -- whitespace

--- array/#/first.js
+++ array/#/first.js
@@ -4,8 +4,6 @@ var firstIndex = require('./first-index');
 
 module.exports = function () {
 	var i;
-	if ((i = firstIndex.call(this)) !== null) {
-		return this[i];
-	}
+	if ((i = firstIndex.call(this)) !== null) return this[i];
 	return undefined;
 };
