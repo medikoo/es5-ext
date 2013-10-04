cb81c0ae00385b0c369c92d76b8057d6a9bcec88
Mariusz Nowak <mariusz@medikoo.com>
2013-10-04 17:03:58 +0200

    No need for outer module

--- test/object/is-copy.js
+++ test/object/is-copy.js
@@ -12,7 +12,6 @@ module.exports = function (t, a) {
 	a(t("raz", "dwa"), false, "String: diff");
 	a(t("raz", "raz"), true, "String: same");
 	a(t("32", 32), false, "String & Number");
-	a(t("raz", new String("raz")), true, "String & String object"); //jslint: skip
 
 	a(t([1, 'raz', true], [1, 'raz', true]), true, "Array: same");
 	a(t([1, 'raz', undefined], [1, 'raz']), false, "Array: diff");
