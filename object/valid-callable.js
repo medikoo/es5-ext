804a3dd5dc2f679f50d531fa7ac1579a85ca32cc
Mariusz Nowak <mariusz@medikoo.com>
2013-10-07 17:57:25 +0200

    -- whitespace

--- object/valid-callable.js
+++ object/valid-callable.js
@@ -3,8 +3,6 @@
 var isCallable = require('./is-callable');
 
 module.exports = function (fn) {
-	if (!isCallable(fn)) {
-		throw new TypeError(fn + " is not a function");
-	}
+	if (!isCallable(fn)) throw new TypeError(fn + " is not a function");
 	return fn;
 };
