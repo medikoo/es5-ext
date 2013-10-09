0d8eba5be11835a1229d59d6a39b606063025543
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 13:32:02 +0200

    -- whitespace

--- function/valid-function.js
+++ function/valid-function.js
@@ -3,8 +3,6 @@
 var isFunction = require('./is-function');
 
 module.exports = function (x) {
-	if (!isFunction(x)) {
-		throw new TypeError(x + " is not a function");
-	}
+	if (!isFunction(x)) throw new TypeError(x + " is not a function");
 	return x;
 };
