fcc13733342e7ca7326bba8163421c226d9dbcae
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 17:15:20 +0200

    -- whitespace

--- string/#/hyphen-to-camel.js
+++ string/#/hyphen-to-camel.js
@@ -5,6 +5,4 @@ var replace = String.prototype.replace
   , re = /-([a-z0-9])/g
   , toUpperCase = function (m, a) { return a.toUpperCase(); };
 
-module.exports = function () {
-	return replace.call(this, re, toUpperCase);
-};
+module.exports = function () { return replace.call(this, re, toUpperCase); };
