5e15bb45c31da1f0e079cfcf100014e9176daffb
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 17:15:40 +0200

    -- whitespace

--- string/#/indent.js
+++ string/#/indent.js
@@ -3,7 +3,6 @@
 var repeat = require('./repeat')
 
   , replace = String.prototype.replace
-
   , re = /(\r\n|[\n\r\u2028\u2029])([\u0000-\u0009\u000b-\uffff]+)/g;
 
 module.exports = function (indent/*, count*/) {
