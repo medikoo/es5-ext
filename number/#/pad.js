80a47af13958c07620b110c1d24566e6386d4b49
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 16:47:45 +0200

    Negative pad doesn't make sense for numbers

--- number/#/pad.js
+++ number/#/pad.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var pad   = require('../../string/#/pad')
-  , toInt = require('../to-int')
+  , toUint = require('../to-uint')
 
   , toFixed = Number.prototype.toFixed;
 
 module.exports = function (length/*, precision*/) {
 	var precision;
-	length = toInt(length);
-	precision = toInt(arguments[1]);
+	length = toUint(length);
+	precision = toUint(arguments[1]);
 
 	return pad.call(precision ? toFixed.call(this, precision) : this,
 		'0', length + (precision ? (1 + precision) : 0));
