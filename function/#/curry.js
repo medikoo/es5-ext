74fe9877ff512a7dac013d9bc12a7d80e337d015
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 15:53:32 +0200

    Assure correct length in Function#curry results

--- function/#/curry.js
+++ function/#/curry.js
@@ -1,19 +1,20 @@
 'use strict';
 
-var toUint   = require('../../number/to-uint')
-  , callable = require('../../object/valid-callable')
+var toUint       = require('../../number/to-uint')
+  , callable     = require('../../object/valid-callable')
+  , defineLength = require('../_define-length')
 
   , slice = Array.prototype.slice, apply = Function.prototype.apply
   , curry;
 
 curry = function self(fn, length, preArgs) {
-	return function () {
+	return defineLength(function () {
 		var args = preArgs ?
 				preArgs.concat(slice.call(arguments, 0, length - preArgs.length)) :
 				slice.call(arguments, 0, length);
 		return (args.length === length) ? apply.call(fn, this, args) :
 				self(fn, length, args);
-	};
+	}, preArgs ? (length - preArgs.length) : length);
 };
 
 module.exports = function (/*length*/) {
