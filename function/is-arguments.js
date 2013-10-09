150915c6a68b119af139b783c2ec6e1bd5020af9
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 13:28:15 +0200

    -- whitespace

--- function/is-arguments.js
+++ function/is-arguments.js
@@ -4,6 +4,4 @@ var toString = Object.prototype.toString
 
   , id = toString.call((function () { return arguments; }()));
 
-module.exports = function (x) {
-	return toString.call(x) === id;
-};
+module.exports = function (x) { return (toString.call(x) === id); };
