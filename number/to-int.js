219f485aa01451b5ea1931157a00c76f2d1c7b6d
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 16:42:58 +0200

    -- whitespace

--- number/to-int.js
+++ number/to-int.js
@@ -5,13 +5,8 @@ var sign = require('../math/sign')
   , abs = Math.abs, floor = Math.floor;
 
 module.exports = function (value) {
-	if (isNaN(value)) {
-		return 0;
-	}
+	if (isNaN(value)) return 0;
 	value = Number(value);
-	if ((value === 0) || !isFinite(value)) {
-		return value;
-	}
-
+	if ((value === 0) || !isFinite(value)) return value;
 	return sign(value) * floor(abs(value));
 };
