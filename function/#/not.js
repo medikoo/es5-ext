c983204b4ed3228cc37879eff9414258f0ca1d37
Mariusz Nowak <mariusz@medikoo.com>
2013-10-09 16:14:53 +0200

    Assure correct length in Function#not

--- function/#/not.js
+++ function/#/not.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var callable = require('../../object/valid-callable')
+var callable     = require('../../object/valid-callable')
+  , defineLength = require('../_define-length')
 
   , apply = Function.prototype.apply;
 
 module.exports = function () {
 	var fn = callable(this);
 
-	return function () {
+	return defineLength(function () {
 		return !apply.call(fn, this, arguments);
-	};
+	}, fn.length);
 };
