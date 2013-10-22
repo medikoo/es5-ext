4a86cbb195b851590493d55257dc113d0f9a37b7
Mariusz Nowak <mariusz@medikoo.com>
2013-10-22 11:24:52 +0200

    Reorganization

--- array/_is-subclassable.js
+++ array/_is-subclassable.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var setPrototypeOf = require('../object/set-prototype-of')
+var setPrototypeOf = require('../object/set-prototype-of');
 
-  , SubArrayTest, arr;
+module.exports = (function () {
+	var SubArrayTest, arr;
 
-if (!setPrototypeOf) {
-	module.exports = false;
-} else {
+	if (!setPrototypeOf) return false;
 	SubArrayTest = function () { Array.apply(this, arguments); };
 	setPrototypeOf(SubArrayTest, Array);
 	SubArrayTest.prototype = Object.create(Array.prototype, {
@@ -14,10 +13,9 @@ if (!setPrototypeOf) {
 			configurable: true }
 	});
 	arr = new SubArrayTest();
-	if (!(arr instanceof SubArrayTest)) {
-		module.exports = false;
-	} else {
-		arr[34] = 'foo';
-		module.exports = (arr.length === 35);
-	}
-}
+	if (!Array.isArray(arr)) return false;
+	if (!(arr instanceof SubArrayTest)) return false;
+
+	arr[34] = 'foo';
+	return (arr.length === 35);
+}());
