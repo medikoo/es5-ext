220d9a162c1372ea0c188d10abc1362017294672
Mariusz Nowak <mariusz@medikoo.com>
2013-10-26 11:26:56 +0200

    Array.from produces instances of `this`

--- array/from.js
+++ array/from.js
@@ -1,20 +1,38 @@
 'use strict';
 
-var isArguments = require('../function/is-arguments')
-  , callable    = require('../object/valid-callable')
-  , validValue  = require('../object/valid-value')
+var isArguments    = require('../function/is-arguments')
+  , callable       = require('../object/valid-callable')
+  , validValue     = require('../object/valid-value')
 
-  , map = Array.prototype.map, slice = Array.prototype.slice;
+  , call = Function.prototype.call
+  , hasOwnProperty = Object.prototype.hasOwnProperty;
 
 module.exports = function (arrayLike/*, mapFn, thisArg*/) {
-	var mapFn = arguments[1], thisArg = arguments[2];
+	var mapFn = arguments[1], thisArg = arguments[2], Constructor, i, arr, l;
 
 	validValue(arrayLike);
-	if (mapFn != null) return map.call(arrayLike, callable(mapFn), thisArg);
 
-	if (isArguments(arrayLike)) {
-		return (arrayLike.length === 1) ? [arrayLike[0]] :
-				Array.apply(null, arrayLike);
+	if (!this || !Array.isPrototypeOf(this)) Constructor = Array;
+	else Constructor = this;
+
+	l = arrayLike.length >>> 0;
+	if (mapFn != null) {
+		callable(mapFn);
+		arr = new Constructor(l);
+		for (i = 0; i < l; ++i) {
+			if (!hasOwnProperty.call(arrayLike, i)) continue;
+			arr[i] = call.call(mapFn, thisArg, arrayLike[i]);
+		}
+		return arr;
 	}
-	return slice.call(arrayLike);
+
+	if ((l !== 1) && isArguments(arrayLike)) {
+		return Constructor.apply(null, arrayLike);
+	}
+	arr = new Constructor(l);
+	for (i = 0; i < l; ++i) {
+		if (!hasOwnProperty.call(arrayLike, i)) continue;
+		arr[i] = arrayLike[i];
+	}
+	return arr;
 };
