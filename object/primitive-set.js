c5f732004fb2ab8c558865c5f1776323944598c4
Mariusz Nowak <mariusz@medikoo.com>
2013-11-11 10:47:37 +0100

    Define signature of primitiveSet

--- object/primitive-set.js
+++ object/primitive-set.js
@@ -2,7 +2,7 @@
 
 var forEach = Array.prototype.forEach, create = Object.create;
 
-module.exports = function () {
+module.exports = function (arg/*, …args*/) {
 	var set = create(null);
 	forEach.call(arguments, function (name) { set[name] = true; });
 	return set;
