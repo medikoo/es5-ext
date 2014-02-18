faf0ed34b37a2f00aa39d22165938c52ab86eee4
Mariusz Nowak <mariusz@medikoo.com>
2014-02-18 18:29:01 +0100

    Add (as introduced in ES6) Math.fround

--- math/index.js
+++ math/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = {
-	acosh: require('./acosh'),
-	asinh: require('./asinh'),
-	atanh: require('./atanh'),
-	cbrt:  require('./cbrt'),
-	cosh:  require('./cosh'),
-	expm1: require('./expm1'),
-	sign:  require('./sign')
+	acosh:  require('./acosh'),
+	asinh:  require('./asinh'),
+	atanh:  require('./atanh'),
+	cbrt:   require('./cbrt'),
+	cosh:   require('./cosh'),
+	expm1:  require('./expm1'),
+	fround: require('./fround'),
+	sign:   require('./sign')
 };
