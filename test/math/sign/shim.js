c345ee31db97097bb283127503263ec3e5bc5c63
Mariusz Nowak <mariusz@medikoo.com>
2014-02-19 09:48:58 +0100

    Fallback Math.sign to native if possible

--- test/math/sign/shim.js
+++ test/math/sign/shim.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var is = require('../../object/is');
+var is = require('../../../object/is');
 
 module.exports = function (t, a) {
 	a(is(t(0), +0), true, "+0");
 	a(is(t(-0), -0), true, "-0");
-	a(is(t({}), NaN), true, "NaN");
+	a(t({}), NaN, true, "NaN");
 	a(t(-234234234), -1, "Negative");
 	a(t(234234234), 1, "Positive");
 };
