d6ac9b3b22982158adb819fc74a8eb5e93a2fe6e
Mariusz Nowak <mariusz@medikoo.com>
2014-02-19 20:17:32 +0100

    Seclude SubArray test type

--- array/_is-subclassable.js
+++ array/_is-subclassable.js
@@ -1,20 +1,12 @@
 'use strict';
 
-var setPrototypeOf = require('../object/set-prototype-of');
-
 module.exports = (function () {
-	var SubArrayTest, arr;
+	var SubArray = require('./_sub-array'), arr;
 
-	if (!setPrototypeOf) return false;
-	SubArrayTest = function () { Array.apply(this, arguments); };
-	setPrototypeOf(SubArrayTest, Array);
-	SubArrayTest.prototype = Object.create(Array.prototype, {
-		constructor: { value: SubArrayTest, enumerable: false, writable: true,
-			configurable: true }
-	});
-	arr = new SubArrayTest();
+	if (!SubArray) return false;
+	arr = new SubArray();
 	if (!Array.isArray(arr)) return false;
-	if (!(arr instanceof SubArrayTest)) return false;
+	if (!(arr instanceof SubArray)) return false;
 
 	arr[34] = 'foo';
 	return (arr.length === 35);
