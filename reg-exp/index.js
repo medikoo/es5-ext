9e9152a90b4d4b8c195920c4e85b9e19badcf186
Mariusz Nowak <mariusz@medikoo.com>
2014-02-19 18:10:00 +0100

    Add (as introduced with ES6) RegExp#match

--- reg-exp/index.js
+++ reg-exp/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+	'#':         require('./#'),
 	escape:      require('./escape'),
 	isRegExp:    require('./is-reg-exp'),
 	validRegExp: require('./valid-reg-exp')
