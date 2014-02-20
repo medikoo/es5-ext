03e7217e1965dbf218319a5615ee1e2da8ed1d29
Mariusz Nowak <mariusz@medikoo.com>
2014-02-20 12:21:52 +0100

    Spec compliant `Object.assign`

--- object/assign/shim.js
+++ object/assign/shim.js
@@ -4,7 +4,15 @@ var keys  = require('../keys')
   , value = require('../valid-value');
 
 module.exports = function (dest, src) {
+	var error;
 	dest = Object(value(dest));
-	keys(src).forEach(function (key) { dest[key] = src[key]; });
+	keys(src).forEach(function (key) {
+		try {
+			dest[key] = src[key];
+		} catch (e) {
+			if (!error) error = e;
+		}
+	});
+	if (error !== undefined) throw error;
 	return dest;
 };
