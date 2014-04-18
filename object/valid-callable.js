eeee395d866c1c08a15a778bb9cba14a09889ba6
Mariusz Nowak <mariusz@medikoo.com>
2014-04-18 18:29:03 +0200

    Do not rely on isCallable in validCallable

--- object/valid-callable.js
+++ object/valid-callable.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var isCallable = require('./is-callable');
-
 module.exports = function (fn) {
-	if (!isCallable(fn)) throw new TypeError(fn + " is not a function");
+	if (typeof fn !== 'function') throw new TypeError(fn + " is not a function");
 	return fn;
 };
