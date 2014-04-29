9e98cd196c9a96d18409838852791b526723ec1d
Mariusz Nowak <mariusz@medikoo.com>
2014-04-29 17:38:41 +0200

    Assure same list contents

--- test/array/__scopes.js
+++ test/array/__scopes.js
@@ -1,9 +1,9 @@
 'use strict';
 
-exports.Array = [1, 2, 3];
+exports.Array = ['1', '2', '3'];
 
 exports.Arguments = (function () {
 	return arguments;
-}(1, 2, 3));
+}('1', '2', '3'));
 
 exports.String = "123";
