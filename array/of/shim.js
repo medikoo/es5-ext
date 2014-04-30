b2ade00c4c7bc5147760d16da86fc687476ad5ef
Mariusz Nowak <mariusz@medikoo.com>
2014-04-30 21:32:04 +0200

    Fix Array.of implementation
    Non array constructor calls where not handled per spec
    Thanks @mathiasbynens

--- array/of/shim.js
+++ array/of/shim.js
@@ -1,5 +1,19 @@
 'use strict';
 
-var slice = Array.prototype.slice;
+var isFunction = require('../../function/is-function')
 
-module.exports = function (/*…items*/) { return slice.call(arguments); };
+  , slice = Array.prototype.slice, defineProperty = Object.defineProperty
+  , desc = { configurable: true, enumerable: true, writable: true, value: null };
+
+module.exports = function (/*…items*/) {
+	var result, i, l;
+	if (!this || (this === Array) || !isFunction(this)) return slice.call(arguments);
+	result = new this(l = arguments.length);
+	for (i = 0; i < l; ++i) {
+		desc.value = arguments[i];
+		defineProperty(result, i, desc);
+	}
+	desc.value = null;
+	result.length = l;
+	return result;
+};
