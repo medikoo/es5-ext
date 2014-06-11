2dac93c4333c66399e7e633414d44119a777422c
Mariusz Nowak <mariusz@medikoo.com>
2014-06-12 00:38:05 +0200

    Fix camelToHyphen for tokens which end with digit

--- string/#/camel-to-hyphen.js
+++ string/#/camel-to-hyphen.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var replace = String.prototype.replace
-  , re = /([a-z])([A-Z])/g;
+  , re = /([a-z\d])([A-Z])/g;
 
 module.exports = function () {
 	return replace.call(this, re, "$1-$2").toLowerCase();
