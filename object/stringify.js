f531f3f011f214ab425b768cc95368bbefcf8a95
Mariusz Nowak <medyk@medikoo.com>
2015-01-20 10:01:00 +0100

    Serialize sparse array properly

--- object/stringify.js
+++ object/stringify.js
@@ -7,6 +7,15 @@ var toArray  = require('./to-array')
   , isArray = Array.isArray, stringify = JSON.stringify
   , keyValueToString = function (value, key) { return stringify(key) + ':' + exports(value); };
 
+var sparseMap = function (arr) {
+	var i, l = arr.length, result = new Array(l);
+	for (i = 0; i < l; ++i) {
+		if (!arr.hasOwnProperty(i)) continue;
+		result[i] = exports(arr[i]);
+	}
+	return result;
+};
+
 module.exports = exports = function (obj) {
 	if (obj == null) return String(obj);
 	switch (typeof obj) {
@@ -17,7 +26,7 @@ module.exports = exports = function (obj) {
 	case 'function':
 		return String(obj);
 	case 'object':
-		if (isArray(obj)) return '[' + obj.map(exports) + ']';
+		if (isArray(obj)) return '[' + sparseMap(obj) + ']';
 		if (isRegExp(obj)) return String(obj);
 		if (isDate(obj)) return 'new Date(' + obj.valueOf() + ')';
 		return '{' + toArray(obj, keyValueToString) + '}';
