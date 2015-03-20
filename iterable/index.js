4982cea86910fddc6a3f83bd6f10ec0fcf4466c7
Mariusz Nowak <medyk@medikoo.com>
2015-03-20 17:00:36 +0100

    forEach for array-likes and iterables

--- iterable/index.js
+++ iterable/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+	forEach:        require('./for-each'),
 	is:             require('./is'),
 	validate:       require('./validate'),
 	validateObject: require('./validate-object')
