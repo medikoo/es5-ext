91d151a485ca1c4e0c39f91359427d2ee2f5505c
Mariusz Nowak <medyk@medikoo.com>
2015-04-10 14:55:10 +0200

    Add better tests for sinh and cosh

--- test/math/cosh/shim.js
+++ test/math/cosh/shim.js
@@ -6,4 +6,8 @@ module.exports = function (t, a) {
 	a(t(Infinity), Infinity, "Infinity");
 	a(t(-Infinity), Infinity, "-Infinity");
 	a(t(1), 1.5430806348152437, "1");
+	a(t(Number.MAX_VALUE), Infinity);
+	a(t(-Number.MAX_VALUE), Infinity);
+	a(t(Number.MIN_VALUE), 1);
+	a(t(-Number.MIN_VALUE), 1);
 };
--- test/math/sinh/shim.js
+++ test/math/sinh/shim.js
@@ -6,4 +6,8 @@ module.exports = function (t, a) {
 	a(t(Infinity), Infinity, "Infinity");
 	a(t(-Infinity), -Infinity, "-Infinity");
 	a(t(1), 1.1752011936438014, "1");
+	a(t(Number.MAX_VALUE), Infinity);
+	a(t(-Number.MAX_VALUE), -Infinity);
+	a(t(Number.MIN_VALUE), 0);
+	a(t(-Number.MIN_VALUE), 0);
 };
