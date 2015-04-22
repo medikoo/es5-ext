6f02b29f7cdae01625f8006d809bebee9752a4af
Mariusz Nowak <medyk@medikoo.com>
2015-04-22 11:23:50 +0200

    Fix Math.sinh implementation

--- test/math/sinh/shim.js
+++ test/math/sinh/shim.js
@@ -8,6 +8,6 @@ module.exports = function (t, a) {
 	a(t(1), 1.1752011936438014, "1");
 	a(t(Number.MAX_VALUE), Infinity);
 	a(t(-Number.MAX_VALUE), -Infinity);
-	a(t(Number.MIN_VALUE), 0);
-	a(t(-Number.MIN_VALUE), 0);
+	a(t(Number.MIN_VALUE), 5e-324);
+	a(t(-Number.MIN_VALUE), -5e-324);
 };
