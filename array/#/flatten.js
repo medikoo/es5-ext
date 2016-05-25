77a0d785c1c0a702359e7084809c0fd706f53688
Mariusz Nowak <medyk@medikoo.com>
2016-05-25 17:59:23 +0200

    Prevent eventual RangeError exceptions

--- array/#/flatten.js
+++ array/#/flatten.js
@@ -6,7 +6,20 @@ var isArray = Array.isArray, forEach = Array.prototype.forEach
 module.exports = function flatten() {
 	var r = [];
 	forEach.call(this, function (x) {
-		push.apply(r, isArray(x) ? flatten.call(x) : [x]);
+		if (isArray(x)) {
+			x = flatten.call(x);
+			if (x.length > 50000) {
+				// Prevent going beyond maximum call stack size eventually
+				while (x.length) {
+					push.apply(r, x.slice(0, 5000));
+					x = x.slice(50000);
+				}
+			} else {
+				push.apply(r, x);
+			}
+		} else {
+			r.push(x);
+		}
 	});
 	return r;
 };
