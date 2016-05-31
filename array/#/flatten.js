fb4629202fea5e80264a614bfcd4928308c08533
Mariusz Nowak <medyk@medikoo.com>
2016-05-31 10:49:08 +0200

    Better alternative for push.apply

--- array/#/flatten.js
+++ array/#/flatten.js
@@ -1,22 +1,12 @@
 'use strict';
 
-var isArray = Array.isArray, forEach = Array.prototype.forEach
-  , push = Array.prototype.push;
+var isArray = Array.isArray, forEach = Array.prototype.forEach;
 
 module.exports = function flatten() {
 	var r = [];
 	forEach.call(this, function (x) {
 		if (isArray(x)) {
-			x = flatten.call(x);
-			if (x.length > 50000) {
-				// Prevent going beyond maximum call stack size eventually
-				while (x.length) {
-					push.apply(r, x.slice(0, 5000));
-					x = x.slice(50000);
-				}
-			} else {
-				push.apply(r, x);
-			}
+			r = r.concat(flatten.call(x));
 		} else {
 			r.push(x);
 		}
