f7a7f54ebe989436afa9739603d57df6708386a9
Mariusz Nowak <medyk@medikoo.com>
2016-10-24 14:16:06 +0200

    Fix Array/#/concat

--- test/array/#/concat/shim.js
+++ test/array/#/concat/shim.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var SubArray = require('../../../../array/_sub-array-dummy-safe');
+var isConcatSpreadable = require('es6-symbol').isConcatSpreadable
+  , SubArray           = require('../../../../array/_sub-array-dummy-safe');
 
 module.exports = function (t, a) {
 	var arr = [1, 3, 45], x = {}, subArr, subArr2, result;
@@ -16,11 +17,10 @@ module.exports = function (t, a) {
 	a.deep(result, ['lol', 'miszko', 'df', 1, 3, 45, 'fef', 'elo', 'fol', null],
 		"Spreable by default");
 
-	SubArray.prototype['@@isConcatSpreadable'] = false;
+	SubArray.prototype[isConcatSpreadable] = false;
 
 	result = t.call(subArr, 'df', arr, 'fef', subArr2, null);
-	a.deep(result, ['lol', 'miszko', 'df', 1, 3, 45, 'fef', subArr2, null],
-		"Non spreadable");
+	a.deep(result, [subArr, 'df', 1, 3, 45, 'fef', subArr2, null], "Non spreadable");
 
-	delete SubArray.prototype['@@isConcatSpreadable'];
+	delete SubArray.prototype[isConcatSpreadable];
 };
