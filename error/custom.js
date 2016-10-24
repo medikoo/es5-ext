377539c9ee0a89e0cf2219cf57619c2644c1a7b3
Mariusz Nowak <medyk@medikoo.com>
2016-10-24 13:10:27 +0200

    Provide message to error at initilization
    Otherwise it may not be exposed in stack

--- error/custom.js
+++ error/custom.js
@@ -5,7 +5,7 @@ var assign = require('../object/assign')
   , captureStackTrace = Error.captureStackTrace;
 
 exports = module.exports = function (message/*, code, ext*/) {
-	var err = new Error(), code = arguments[1], ext = arguments[2];
+	var err = new Error(message), code = arguments[1], ext = arguments[2];
 	if (ext == null) {
 		if (code && (typeof code === 'object')) {
 			ext = code;
