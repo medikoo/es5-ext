5512b43443fb2c3284bd1d13d6a925434a1cda05
Mariusz Nowak <medyk@medikoo.com>
2017-01-06 16:49:53 +0100

    Optimize flatten algorithm

--- array/#/flatten.js
+++ array/#/flatten.js
@@ -2,14 +2,11 @@
 
 var isArray = Array.isArray, forEach = Array.prototype.forEach;
 
-module.exports = function flatten() {
+module.exports = function () {
 	var r = [];
-	forEach.call(this, function (x) {
-		if (isArray(x)) {
-			r = r.concat(flatten.call(x));
-		} else {
-			r.push(x);
-		}
+	forEach.call(this, function self(x) {
+		if (isArray(x)) x.forEach(self);
+		else r.push(x);
 	});
 	return r;
 };
