9577e95c547ba1136acc37c43df269c29b5550b6
Mariusz Nowak <medyk@medikoo.com>
2017-01-12 10:45:14 +0100

    Improve tests

--- test/array/#/flatten.js
+++ test/array/#/flatten.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var o = [1, 2, [3, 4, [5, 6], 7, 8], 9, 10];
+var o = [1, 2, [3, 4, [5, 6], 7, 8], 9, 10, [11, 12], 13];
 
 module.exports = {
 	__generic: function (t, a) {
 		a(t.call(this).length, 3);
 	},
 	"Nested Arrays": function (t, a) {
-		a(t.call(o).length, 10);
+		var result = t.call(o);
+		a.not(o, result);
+		a.deep(result, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
 	}
 };
