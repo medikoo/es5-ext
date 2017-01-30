85e6ac6af72bb8def5ff0fcf2cb81277b33b34a3
Mariusz Nowak <medyk@medikoo.com>
2017-01-30 11:21:39 +0100

    Ensure proper handling of sparse arrays

--- array/#/flatten.js
+++ array/#/flatten.js
@@ -3,7 +3,8 @@
 'use strict';
 
 var ensureValue = require('../../object/valid-value')
-  , isArray     = Array.isArray;
+
+  , isArray = Array.isArray, hasOwnProperty = Object.prototype.hasOwnProperty;
 
 module.exports = function () {
 	var input = ensureValue(this), index = 0, remaining, remainingIndexes, l, i, result = [];
@@ -11,6 +12,7 @@ module.exports = function () {
 	while (input) {
 		l = input.length;
 		for (i = index; i < l; ++i) {
+			if (!hasOwnProperty.call(input, i)) continue;
 			if (isArray(input[i])) {
 				if (i < (l - 1)) {
 					if (!remaining) {
