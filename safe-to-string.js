3c5cd12a549dce623ca66225ba7655f67dcb4ce3
Mariusz Nowak <medyk@medikoo.com>
2017-05-15 12:00:39 +0200

    fix: take all changes in safeToString
    In early ES6 implementations String(symbol) crashed while symbol.toString() returned string as expected

--- safe-to-string.js
+++ safe-to-string.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var isCallable = require('./object/is-callable');
+
 module.exports = function (value) {
 	try {
+		if (value && isCallable(value.toString)) return value.toString();
 		return String(value);
 	} catch (e) {
 		return "<non-stringifiable value>";
