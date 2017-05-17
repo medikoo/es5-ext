5442279a644969605791b22b91bbd1cb2bca289f
Mariusz Nowak <medyk@medikoo.com>
2017-05-17 16:56:01 +0200

    feat: support propertyNames option in Object.copy

--- object/copy.js
+++ object/copy.js
@@ -1,10 +1,21 @@
-'use strict';
+"use strict";
 
-var assign = require('./assign')
-  , value  = require('./valid-value');
+var aFrom                = require("../array/from")
+  , assign               = require("./assign")
+  , value                = require("./valid-value")
+  , propertyIsEnumerable = Object.prototype.propertyIsEnumerable;
 
-module.exports = function (obj) {
-	var copy = Object(value(obj));
-	if (copy !== obj) return copy;
-	return assign({}, obj);
+module.exports = function(obj /*, propertyNames*/) {
+	var copy = Object(value(obj)), propertyNames = arguments[1];
+	if (copy !== obj && !propertyNames) return copy;
+	var result = {};
+	if (propertyNames) {
+		aFrom(propertyNames, function(propertyName) {
+			if (propertyIsEnumerable.call(obj, propertyName))
+				result[propertyName] = obj[propertyName];
+		});
+	} else {
+		assign(result, obj);
+	}
+	return result;
 };
