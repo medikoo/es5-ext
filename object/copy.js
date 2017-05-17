f1d11795b1ec0068a3d151222fa1332ab13996dd
Mariusz Nowak <medyk@medikoo.com>
2017-05-17 17:03:40 +0200

    style: fix lint issues

--- object/copy.js
+++ object/copy.js
@@ -1,18 +1,17 @@
-"use strict";
+'use strict';
 
-var aFrom                = require("../array/from")
-  , assign               = require("./assign")
-  , value                = require("./valid-value")
+var aFrom                = require('../array/from')
+  , assign               = require('./assign')
+  , value                = require('./valid-value')
   , propertyIsEnumerable = Object.prototype.propertyIsEnumerable;
 
-module.exports = function(obj /*, propertyNames*/) {
+module.exports = function (obj/*, propertyNames*/) {
 	var copy = Object(value(obj)), propertyNames = arguments[1];
 	if (copy !== obj && !propertyNames) return copy;
 	var result = {};
 	if (propertyNames) {
-		aFrom(propertyNames, function(propertyName) {
-			if (propertyIsEnumerable.call(obj, propertyName))
-				result[propertyName] = obj[propertyName];
+		aFrom(propertyNames, function (propertyName) {
+			if (propertyIsEnumerable.call(obj, propertyName)) result[propertyName] = obj[propertyName];
 		});
 	} else {
 		assign(result, obj);
