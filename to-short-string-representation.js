6842d067556ad60adba6df9b657a4fc87bccc8e9
Mariusz Nowak <medyk@medikoo.com>
2017-08-18 12:10:51 +0200

    feat: toShortStringRepresentation util

---
+++ to-short-string-representation.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var safeToString = require("./safe-to-string");
+
+var reNewLine = /[\n\r\u2028\u2029]/g;
+
+module.exports = function (value) {
+	var string = safeToString(value);
+	// Trim if too long
+	if (string.length > 100) string = string.slice(0, 99) + "…";
+	// Replace eventual new lines
+	string = string.replace(reNewLine, " ");
+	return string;
+};
