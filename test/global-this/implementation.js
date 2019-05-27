6a8f571fadb792880a8a7ee41a7e85fe180f322f
Mariusz Nowak <mariusz@medikoo.com>
2019-05-27 17:13:58 +0200

    test: fix title

--- test/global-this/implementation.js
+++ test/global-this/implementation.js
@@ -4,7 +4,7 @@ var isObject   = require("type/object/is")
   , assert     = require("chai").assert
   , globalThis = require("../../global-this/implementation");
 
-describe("globalThis", function () {
+describe("global-this/implementation", function () {
 	it("Should be an object", function () { assert(isObject(globalThis)); });
 	it("Should be a global object", function () { assert.equal(globalThis.Array, Array); });
 	it("Internal resolution should not introduce side-effects", function () {
