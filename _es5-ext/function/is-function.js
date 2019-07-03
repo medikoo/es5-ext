bf746cf4d2a4142d901a9a37f4b556231fb67b4d
Mariusz Nowak <mariusz@medikoo.com>
2019-07-03 15:53:59 +0200

    style: Prettify

--- _es5-ext/function/is-function.js
+++ _es5-ext/function/is-function.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var objToString = Object.prototype.toString, id = objToString.call(require("./noop"));
+var objToString = Object.prototype.toString
+  , id          = objToString.call(require("./noop"));
 
 module.exports = function (value) {
 	return typeof value === "function" && objToString.call(value) === id;
