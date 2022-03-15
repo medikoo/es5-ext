e91302381556b2851f6d8cba51c635a2a8b37f0c
Egor Muravyov <emuravyov@wiley.com>
2022-03-15 15:08:47 +0400

    Revert "style: Prettify"
    This reverts commit 0313c47497c0b4794ded08ddf50973ed23bb7333.

--- number/is-number.js
+++ number/is-number.js
@@ -5,7 +5,6 @@ var objToString = Object.prototype.toString, id = objToString.call(1);
 module.exports = function (value) {
 	return (
 		typeof value === "number" ||
-		value instanceof Number ||
-		(typeof value === "object" && objToString.call(value) === id)
+		(value instanceof Number || (typeof value === "object" && objToString.call(value) === id))
 	);
 };
