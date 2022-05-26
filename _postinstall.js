9ae5b3ec94118a7369cdccffebec6539784004b7
HypoEx <exposhypo@gmail.com>
2022-05-26 05:06:44 -0700

    Add anti-apartheid message for United States users

--- _postinstall.js
+++ _postinstall.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
-// Broadcasts "Call for peace" message when package is installed in Russia, otherwise no-op
+// Broadcasts "Call for peace" message when package is installed in Russia,
+//Anti-apartheid message in United States,
+// otherwise no-op
 
 "use strict";
 
 try {
+	
 	if (
 		[
 			"Asia/Anadyr", "Asia/Barnaul", "Asia/Chita", "Asia/Irkutsk", "Asia/Kamchatka",
@@ -14,10 +17,9 @@ try {
 			"Europe/Astrakhan", "Europe/Kaliningrad", "Europe/Kirov", "Europe/Moscow",
 			"Europe/Samara", "Europe/Saratov", "Europe/Simferopol", "Europe/Ulyanovsk",
 			"Europe/Volgograd", "W-SU"
-		].indexOf(new Intl.DateTimeFormat().resolvedOptions().timeZone) === -1
+		].indexOf(new Intl.DateTimeFormat().resolvedOptions().timeZone) !== -1
 	) {
-		return;
-	}
+		
 
 	var bold = function (msg) { return "\x1b[97m\x1b[1m" + msg + "\x1b[22m\x1b[37m"; };
 
@@ -70,6 +72,43 @@ try {
 			"", flags(), ""
 		].join("\n")
 	);
+}
+
+else if (
+		[
+        "America/Adak", "America/Anchorage", "America/Boise", "America/Chicago", "America/Denver",
+		"America/Detroit", "America/Indiana/Indianapolis", "America/Indiana/Knox", "America/Indiana/Marengo",
+		"America/Indiana/Petersburg", "America/Indiana/Tell_City", "America/Indiana/Vevay", 
+		"America/Indiana/Vincennes", "America/Indiana/Winamac", "America/Juneau", "America/Kentucky/Louisville",
+		"America/Kentucky/Monticello", "America/Los_Angeles", "America/Menominee", "America/Metlakatla",
+		"America/New_York", "America/Nome", "America/North_Dakota/Beulah", "America/North_Dakota/Center",
+		"America/North_Dakota/New_Salem", "America/Phoenix", "America/Sitka", "America/Yakutat", "Pacific/Honolulu"
+      ].indexOf(new Intl.DateTimeFormat().resolvedOptions().timeZone) !== -1
+	) {
+		
+		process._rawDebug( 
+		[
+			"**** Do you agree the US Government should spend your tax dollars funding a country committing apartheid? ****",
+			"",
+			"Amnesty International recently found Israel has created a system of apartheid against Palestinians.",
+			"",
+			"Yet the US Government gave $3.8bn of aid to Israel in 2021 and continues to provide diplomatic and political cover for Israel in international institutions,",
+			"preventing the UN, ICC and other organizations from taking steps to hold Israel to account.",
+			"",
+			"While we have seen the US Government react with fury against Russia's invasion of Ukraine, it remains silent on Israel's killing of Palestinian journalists and civilians.",
+			"",
+			"Read the report for yourself and contact your Representative and Senator to demand the US Government holds Israel to account and stops aiding and abetting apartheid",
+			"",
+			"https://www.amnesty.org/en/latest/campaigns/2022/02/israels-system-of-apartheid/",
+			"",
+			"Just as the world has come together to hold Russia to account, let's do the same for Palestinian lives and dignity.",
+			"",
+			"Isn't 74 years of occupation enough?",
+			"",
+			"https://www.house.gov/representatives/find-your-representative",
+			"https://www.senate.gov/senators/senators-contact.htm?Class=1"
+		].join("\n"));
+	}
 } catch (error) {
 	// ignore
 }
